91df7d8227b82f1189fdac44d2db57c855e91ae9
kimdaeyeub <kdy3915@naver.com>
2023-10-11 03:19:52 +0900

    Swipe Card 완성

--- lib/screens/flash_card_screen.dart
+++ lib/screens/flash_card_screen.dart
@@ -24,19 +24,6 @@ class _FlashCardScreenState extends State<FlashCardScreen>
   late AnimationController _animationController;
   late Animation _animation;
 
-  @override
-  void initState() {
-    super.initState();
-    _animationController = AnimationController(
-      vsync: this,
-      duration: const Duration(seconds: 1),
-    );
-    _animation = Tween(begin: 0.0, end: 1.0).animate(_animationController)
-      ..addListener(() {
-        setState(() {});
-      });
-  }
-
   late final Tween<double> _rotation = Tween(
     begin: -15,
     end: 15,
@@ -48,14 +35,18 @@ class _FlashCardScreenState extends State<FlashCardScreen>
   );
 
   Color _bgColor = Colors.blue;
+  String _title = "";
 
   void _onHorizontalDragUpdate(DragUpdateDetails details) {
     _position.value += details.delta.dx;
     if (_position.value.isNegative) {
       _bgColor = const Color(0xffFF7951);
+      _title = "Need to review";
     } else {
       _bgColor = Colors.green;
+      _title = "I got it right";
     }
+    setState(() {});
   }
 
   void _whenComplete() {
@@ -65,6 +56,7 @@ class _FlashCardScreenState extends State<FlashCardScreen>
       _factor = 0;
       _bgColor = Colors.blue;
     });
+    _progressAnimationController.forward();
   }
 
   void _onHorizontalDragEnd(DragEndDetails details) {
@@ -74,6 +66,8 @@ class _FlashCardScreenState extends State<FlashCardScreen>
       setState(() {
         _factor = _position.value.isNegative ? -1 : 1;
       });
+      _animationController.reset();
+      _title = "";
       _position
           .animateTo(
             (dropZone) * _factor,
@@ -82,6 +76,8 @@ class _FlashCardScreenState extends State<FlashCardScreen>
           .whenComplete(_whenComplete);
     } else {
       _bgColor = Colors.blue;
+      _title = "";
+
       _position.animateTo(
         0,
         curve: Curves.easeOut,
@@ -98,11 +94,11 @@ class _FlashCardScreenState extends State<FlashCardScreen>
   int _index = 0;
 
   final _texts = [
-    "Hello",
-    "im fine",
-    "and you?",
-    "i",
-    "me",
+    {"question": "한국의 수도", "answer": "서울"},
+    {"question": "세상에서 가장 쉬운 숫자는?", "answer": "190000(십구만)"},
+    {"question": "날마다 흑심을 품고 다니는 것은?", "answer": "연필"},
+    {"question": "손님이 뜸하면 돈을 버는 사람은?", "answer": "한의사"},
+    {"question": "오래될수록 젊어 보이는 것은", "answer": "사진"},
   ];
 
   void _onTap() {
@@ -111,7 +107,38 @@ class _FlashCardScreenState extends State<FlashCardScreen>
     } else {
       _animationController.forward();
     }
-    print(_animationController.value);
+  }
+
+  final ValueNotifier<double> _progress = ValueNotifier(0.0);
+
+  late final AnimationController _progressAnimationController =
+      AnimationController(
+    vsync: this,
+    duration: const Duration(
+      milliseconds: 300,
+    ),
+  );
+
+  double _double = 0.0;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+    _animation = Tween(begin: 0.0, end: 1.0).animate(_animationController)
+      ..addListener(() {
+        setState(() {});
+      });
+    _progressAnimationController.addListener(() {
+      _progress.value = _double + _progressAnimationController.value * 0.2;
+      if (_progressAnimationController.isCompleted) {
+        _double = _index.toDouble() * 0.2;
+        _progressAnimationController.reset();
+      }
+    });
   }
 
   @override
@@ -125,7 +152,6 @@ class _FlashCardScreenState extends State<FlashCardScreen>
               ) *
               pi /
               180;
-          final scale = _scale.transform(_position.value.abs() / size.width);
           return Stack(
             alignment: Alignment.center,
             children: [
@@ -135,26 +161,19 @@ class _FlashCardScreenState extends State<FlashCardScreen>
                   color: _bgColor,
                 ),
               ),
-              // Positioned(
-              //   top: 50,
-              //   child: Transform.scale(
-              //     scale: min(scale, 1.0),
-              //     child: Card(
-              //       index: _index == 4
-              //           ? 0
-              //           : _index == 0
-              //               ? 0
-              //               : _index + 1,
-              //       text: _texts[_index == 4
-              //           ? 0
-              //           : _index == 0
-              //               ? 0
-              //               : _index + 1],
-              //     ),
-              //   ),
-              // ),
               Positioned(
-                top: 130,
+                top: 110,
+                child: Text(
+                  _title,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Positioned(
+                top: 180,
                 child: GestureDetector(
                   onHorizontalDragUpdate: _onHorizontalDragUpdate,
                   onHorizontalDragEnd: _onHorizontalDragEnd,
@@ -175,20 +194,13 @@ class _FlashCardScreenState extends State<FlashCardScreen>
                                   transform: Matrix4.rotationY(1 * pi),
                                   child: Card(
                                     index: _index,
-                                    text: _texts[_index == 4
-                                        ? 0
-                                        : _index == 0
-                                            ? 0
-                                            : _index + 1],
+                                    text:
+                                        "답변: ${_texts[_index]["answer"].toString()}",
                                   ),
                                 )
                               : Card(
                                   index: _index,
-                                  text: _texts[_index == 4
-                                      ? 0
-                                      : _index == 0
-                                          ? 0
-                                          : _index + 1],
+                                  text: _texts[_index]["question"]!,
                                 ),
                         ),
                       ),
@@ -196,6 +208,28 @@ class _FlashCardScreenState extends State<FlashCardScreen>
                   ),
                 ),
               ),
+              Positioned(
+                bottom: 0,
+                child: ValueListenableBuilder(
+                  valueListenable: _progress,
+                  builder: (context, value, child) {
+                    return Container(
+                      // width: MediaQuery.of(context).size.width,
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 20,
+                        horizontal: 20,
+                      ),
+                      // height: 80,
+                      child: CustomPaint(
+                        size: const Size(350, 30),
+                        painter: ProgressBarPainter(
+                          value: _progress.value,
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              )
             ],
           );
         },
@@ -221,9 +255,13 @@ class Card extends StatelessWidget {
       elevation: 10,
       borderRadius: BorderRadius.circular(10),
       clipBehavior: Clip.hardEdge,
-      child: SizedBox(
+      child: Container(
         width: size.width * 0.8,
-        height: size.height * 0.65,
+        height: size.height * 0.55,
+        padding: const EdgeInsets.symmetric(
+          horizontal: 20,
+          vertical: 20,
+        ),
         child: Align(
           alignment: Alignment.center,
           child: Text(
@@ -236,5 +274,41 @@ class Card extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+class ProgressBarPainter extends CustomPainter {
+  final double value;
+
+  ProgressBarPainter({required this.value});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    // final bgPaint = Paint()..color = Colors.teal.shade300;
+
+    // final bgRect = Rect.fromLTWH(0, 0, size.width, size.height);
+
+    // canvas.drawRect(bgRect, bgPaint);
+    final bgPaint = Paint()
+      ..color = Colors.grey.shade400
+      ..strokeCap = StrokeCap.round
+      ..strokeWidth = 15;
+
+    final progressPaint = Paint()
+      ..color = Colors.white
+      ..strokeCap = StrokeCap.round
+      ..strokeWidth = 15;
+
+    // final volumeRect = Rect.fromLTWH(0, 0, size.width * value, size.height);
+    // canvas.drawRect(volumeRect, volumePaint);
+
+    canvas.drawLine(Offset.zero, Offset(size.width, 0), bgPaint);
+    canvas.drawLine(Offset.zero, Offset(size.width * value, 0), progressPaint);
+  }
+
+  @override
+  bool shouldRepaint(covariant ProgressBarPainter oldDelegate) {
+    // return oldDelegate.
+    return oldDelegate.value != value;
   }
 }
